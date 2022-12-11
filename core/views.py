5d7022461d920a5b24f92a9eec2a69ef39b4d3a2
Jelson Matheus <jelsonmatheus200f@gmail.com>
2022-12-11 15:06:44 -0300

    Refatora tela index

--- core/views.py
+++ core/views.py
@@ -1,18 +1,24 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class BaseView(LoginRequiredMixin):
+    """
+    Classe base para as demais views que requer um usuário logado.
+    """
     login_url = '/auth/login/'
+
+
+class CoreLoginView(LoginView):
+    """
+    View para login do Diretor ou Professor.
+    """
+    template_name = 'core/registration/login.html'
+    redirect_authenticated_user = True
+    next_page = 'core:index'
 
 
 class IndexView(BaseView, TemplateView):
     template_name = 'core/index.html'
 
-
-class CoreLoginView(LoginView):
-    template_name = 'core/registration/login.html'
-    redirect_authenticated_user = True
-    next_page = 'core:index'
