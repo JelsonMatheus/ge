92a599ea33058d9c02690018bd72bb05ee27da0e
Jelson Matheus <jelsonmatheus200f@gmail.com>
2022-12-11 09:09:02 -0300

    feat: Adiciona model user

--- core/models.py
+++ core/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Usuario(AbstractUser):
+    class TipoUsuario(models.TextChoices):
+        DIRETOR = 'D', _('Diretor')
+        PROFESSOR = 'P', _('Professor')
+
+    cpf = models.CharField(_('CPF'), max_length=11)
+    tipo = models.CharField(max_length=2, choices=TipoUsuario.choices,
+                            default=TipoUsuario.PROFESSOR)
+    
+    def is_diretor(self):
+        return self.tipo == self.TipoUsuario.DIRETOR
 
