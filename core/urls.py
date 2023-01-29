d269c689d11a9dd3e3aea80eae9d1b6f63663ca5
Jelson Matheus <jelsonmatheus200f@gmail.com>
2023-01-29 12:32:51 -0300

    feta: Add pagina 404.html

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from core import views
+from django.conf.urls.static import static
+from ge import settings
+
 
 app_name = 'core'
 
--- ge/settings.py
+++ ge/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,13 +120,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = config('STATIC_URL', default='static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
